13ee492af0941315a4af11f85d4e245d57f8aa82
1985wasagoodyear <yu275@uregina.ca>
2022-01-05 09:45:31 -0600

    Add driver program for reading from the command line

--- StringCalculator/main.swift
+++ StringCalculator/main.swift
@@ -1,1 +1,30 @@
+import Darwin
 
+let instructions = """
+String Calculator
+
+Finds the sum of all numbers within a provided string of numbers, with the following rules:
+* all numbers must be comma-separated
+* numbers greater than 1000 will be ignored
+* newlines will be ignored
+* will not find a sum if any negative numbers exist
+* use the following format if you would like to use a custom separator instead of a comma:
+  //[delimiter]\\n[delimiter separated numbers]
+
+"""
+
+print(instructions)
+print("Please enter your string:")
+
+guard let input = readLine() else {
+    print("No input found.")
+    exit(0)
+}
+
+do {
+    let calculator = StringCalculator()
+    let output = try calculator.add(input)
+    print("Your sum is: \(output)")
+} catch {
+    print(error)
+}
